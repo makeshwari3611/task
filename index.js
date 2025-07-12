d45ff0181acb55f5786d1f31bfa6afbb4ca678e5
makeshwari3611 <makeshwari3611@gmail.com>
2025-07-12 10:43:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var number=4;
+var name=`js`;
+var iscoludy=true;
+console.log(number+" "+name+" "+iscoludy);
+document.writeln(number);
+document.writeln(name);
+document.writeln(iscoludy);
+//task2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscoludy));
+//task3
+var a=parseInt(prompt("Enter the first value"));
+var b=parseInt(prompt("Enter the second value"));
+alert(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
